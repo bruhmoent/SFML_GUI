ad89be06bd289d2b4783713213706855182c9a35
Rob <69918580+bruhmoent@users.noreply.github.com>
2023-03-08 19:37:04 +0100

    Create directives.hpp

---
+++ src/directives.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef DIRECTIVES_HPP
+#define DIRECTIVES_HPP
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+#include <Audio.hpp>
+#include <Graphics.hpp>
+#include <Window.hpp>
+
+#endif
